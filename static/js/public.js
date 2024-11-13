645a8879e061e0a21c893db0ab93a6bb8ee2232d
ksolheim <kristian@solhe.im>
2024-11-13 10:52:46 +0100

    Changed layout

--- static/js/public.js
+++ static/js/public.js
@@ -26,8 +26,8 @@ document.addEventListener('DOMContentLoaded', function() {
                                         <source src="/static/video/Champ_720P60.mp4" type="video/mp4">
                                     </video>
                                 </div>
-                                <p class="text-muted fs-6 text-center"><br><br><br><br>Developed by Altera IT with ❤️</p>
-                                <p class="text-muted fs-6 text-center"><img src="/static/images/logos/github-mark.png" alt="GitHub" style="width: 25x; height: 25px;"><br><a class="text-muted" href="https://github.com/ksolheim/smartarts" style="color: black; text-decoration: none;">ksolheim/smartarts</a></p>
+                                <p class="text-muted fs-6 text-center"><br><br><br><br><img src="/static/images/logos/github-mark.png" alt="GitHub" style="width: 25x; height: 25px;"><br><a class="text-muted" href="https://github.com/ksolheim/smartarts" style="color: black; text-decoration: none;">ksolheim/smartarts</a></p>
+                                <p class="text-muted fs-6 text-center">Developed by Altera IT with ❤️</p>
                             </div>`;
                     } else {
                         const html = `
